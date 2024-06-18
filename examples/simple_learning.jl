54bef442230b2d21ab31602c1e57ab44853701dc
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-06-18 19:06:34 +0200

    Update simple_learning.jl

--- examples/simple_learning.jl
+++ examples/simple_learning.jl
@@ -9,16 +9,18 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.2
 #   kernelspec:
-#     display_name: Julia 1.10.4
+#     display_name: Julia 1.10.3
 #     language: julia
 #     name: julia-1.10
 # ---
 
+# +
 # # Simple learning
 # +
 using OptimalTransportDataIntegration
 using CSV
 using DataFrames
+import Flux
 import Flux: Chain, Dense, softmax, relu, logitcrossentropy
 
 function onehot(x :: AbstractMatrix)
@@ -37,35 +39,26 @@ function onehot(x :: AbstractVector)
     Float32.(permutedims(x) .== levels)
 end
 
+function onecold(x :: AbstractMatrix)
+    argmax.(eachcol(x))
+end
+
 params = DataParameters(nA=1000, nB=1000, mB=[2,0,0], eps=0, p=0.2)
 
 #data = generate_xcat_ycat(params)
 data = CSV.read("data.csv", DataFrame)
-dba = subset(data, :database => ByRow(==(1)))
-dbb = subset(data, :database => ByRow(==(2)))
-YB = dbb.Y
-ZA = dba.Z
-
-XA = onehot(Matrix(dba[!, [:X1, :X2, :X3]]))
-XB = onehot(Matrix(dbb[!, [:X1, :X2, :X3]]))
-
-YA = onehot(dba.Y)
-ZB = onehot(dbb.Z)
-
-dimX = size(XA, 1)
-dimY = size(YA, 1)
-dimZ = size(ZB, 1)
-
-nA = size(XA, 2)
-nB = size(XB, 2)
 # -
 
-function train!(model, x, y)
-    
-    loader = Flux.DataLoader((x, y), batchsize=64, shuffle=true)
-    optim = Flux.setup(Flux.Adam(0.1), model)
 
-    for epoch in 1:1_000
+onecold(onehot(data.Y)) ≈ data.Y
+
+# +
+function train!(model, x, y)
+        
+    loader = Flux.DataLoader((x, y), batchsize=1000, shuffle=true)
+    optim = Flux.setup(Flux.Adam(0.1), model)
+    
+    for epoch in 1:500
         for (x, y) in loader
             grads = Flux.gradient(model) do m
                 y_hat = m(x)
@@ -76,22 +69,44 @@ function train!(model, x, y)
     end
 end
 
-
-# +
-modelXYA = Chain( Dense(dimX, 10, relu), Dense(10, dimY, relu), softmax)
-modelXZB = Chain( Dense(dimX, 10, relu), Dense(10, dimZ, relu), softmax)
-
-@time train!(modelXYA, XA, YA)
-@time train!(modelXZB, XB, ZB)
+function simple_learning( data )
+    
+    dba = subset(data, :database => ByRow(==(1)))
+    dbb = subset(data, :database => ByRow(==(2)))
+    YB = dbb.Y
+    ZA = dba.Z
+    
+    XA = onehot(Matrix(dba[!, [:X1, :X2, :X3]]))
+    XB = onehot(Matrix(dbb[!, [:X1, :X2, :X3]]))
+    
+    YA = onehot(dba.Y)
+    ZB = onehot(dbb.Z)
+    
+    dimX = size(XA, 1)
+    dimY = size(YA, 1)
+    dimZ = size(ZB, 1)
+    
+    nA = size(XA, 2)
+    nB = size(XB, 2)
+    
+    modelXYA = Chain( Dense(dimX, 160, relu), Dense(160, dimY, relu), softmax)
+    modelXZB = Chain( Dense(dimX, 160, relu), Dense(160, dimZ, relu), softmax)
+    
+    train!(modelXYA, XA, YA)
+    train!(modelXZB, XB, ZB)
+    
+    YBpred = onecold(modelXYA(XB))
+    ZApred = onecold(modelXZB(XA))
+    
+    @show sum(YB .== YBpred) / nA
+    @show sum(ZA .== ZApred) / nB
+    return (sum(YB .== YBpred) + sum(ZA .== ZApred)) / (nA + nB)
+    
+end
+    
+@time simple_learning( data )
 # -
-modelB(XA)
 
-# +
-YBpred = argmax.(eachcol(modelA(XB)))
-ZApred = argmax.(eachcol(modelB(XA)))
 
-@show sum(YB .== YBpred) / nA
-@show sum(ZA .== ZApred) / nB
-# -
 
 
