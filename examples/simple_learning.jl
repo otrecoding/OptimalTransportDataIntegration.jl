ff52793c8d81dcb55664a5a11b5dba47ab3615a5
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-06-19 21:51:44 +0200

    Improve two examples

--- examples/simple_learning.jl
+++ examples/simple_learning.jl
@@ -9,19 +9,18 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.2
 #   kernelspec:
-#     display_name: Julia 1.10.3
+#     display_name: Julia 1.10.4
 #     language: julia
 #     name: julia-1.10
 # ---
 
-# +
 # # Simple learning
 # +
 using OptimalTransportDataIntegration
 using CSV
 using DataFrames
 import Flux
-import Flux: Chain, Dense, softmax, relu, logitcrossentropy
+import Flux: Chain, Dense
 
 function onehot(x :: AbstractMatrix)
     res = Vector{Float32}[] 
@@ -34,53 +33,37 @@ function onehot(x :: AbstractMatrix)
     return stack(res, dims=1) 
 end
 
-function onehot(x :: AbstractVector)
-    levels = filter( x -> x != 0, sort(unique(x)))
-    Float32.(permutedims(x) .== levels)
-end
-
-function onecold(x :: AbstractMatrix)
-    argmax.(eachcol(x))
-end
-
-params = DataParameters(nA=1000, nB=1000, mB=[2,0,0], eps=0, p=0.2)
-
-#data = generate_xcat_ycat(params)
-data = CSV.read("data.csv", DataFrame)
-# -
-
-
-onecold(onehot(data.Y)) ≈ data.Y
 
 # +
-function train!(model, x, y)
-        
-    loader = Flux.DataLoader((x, y), batchsize=1000, shuffle=true)
-    optim = Flux.setup(Flux.Adam(0.1), model)
+function train!(model, x, y, epochs = 500)
     
-    for epoch in 1:500
+    loader = Flux.DataLoader((x, y), batchsize=512, shuffle=true)
+    optim = Flux.setup(Flux.Adam(0.01), model)
+    
+    for epoch in 1:epochs
         for (x, y) in loader
             grads = Flux.gradient(model) do m
                 y_hat = m(x)
-                Flux.binarycrossentropy(y_hat, y)
+                Flux.logitcrossentropy(y_hat, y)
             end
             Flux.update!(optim, model, grads[1])
         end
     end
+        
 end
-
+# +
 function simple_learning( data )
-    
+
     dba = subset(data, :database => ByRow(==(1)))
     dbb = subset(data, :database => ByRow(==(2)))
     YB = dbb.Y
     ZA = dba.Z
     
-    XA = onehot(Matrix(dba[!, [:X1, :X2, :X3]]))
-    XB = onehot(Matrix(dbb[!, [:X1, :X2, :X3]]))
+    XA = onehot(Matrix{Float32}(dba[!, [:X1, :X2, :X3]]))
+    XB = onehot(Matrix{Float32}(dbb[!, [:X1, :X2, :X3]]))
     
-    YA = onehot(dba.Y)
-    ZB = onehot(dbb.Z)
+    YA = Flux.onehotbatch(dba.Y, 1:4)
+    ZB = Flux.onehotbatch(dbb.Z, 1:3)
     
     dimX = size(XA, 1)
     dimY = size(YA, 1)
@@ -89,24 +72,23 @@ function simple_learning( data )
     nA = size(XA, 2)
     nB = size(XB, 2)
     
-    modelXYA = Chain( Dense(dimX, 160, relu), Dense(160, dimY, relu), softmax)
-    modelXZB = Chain( Dense(dimX, 160, relu), Dense(160, dimZ, relu), softmax)
+    modelXYA = Chain(Dense(dimX, 10),  Dense(10, dimY))
+    modelXZB = Chain(Dense(dimX, 10),  Dense(10, dimZ))
     
-    train!(modelXYA, XA, YA)
-    train!(modelXZB, XB, ZB)
+    la = train!(modelXYA, XA, YA)
+    lb = train!(modelXZB, XB, ZB)
     
-    YBpred = onecold(modelXYA(XB))
-    ZApred = onecold(modelXZB(XA))
+    YBpred = Flux.onecold(modelXYA(XB))
+    ZApred = Flux.onecold(modelXZB(XA))
     
-    @show sum(YB .== YBpred) / nA
-    @show sum(ZA .== ZApred) / nB
-    return (sum(YB .== YBpred) + sum(ZA .== ZApred)) / (nA + nB)
+    (sum(YB .== YBpred) + sum(ZA .== ZApred)) / (nA + nB)
     
 end
-    
-@time simple_learning( data )
 # -
+data = CSV.read("data.csv", DataFrame)
 
+
+simple_learning( data )
 
 
 
