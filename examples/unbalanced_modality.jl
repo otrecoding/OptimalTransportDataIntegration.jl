95cf890f1a14fe631a805177c680a5673619e7f6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-11-20 15:39:23 +0100

    Update unbalanced_modality.jl

--- examples/unbalanced_modality.jl
+++ examples/unbalanced_modality.jl
@@ -23,10 +23,45 @@ import PythonOT
 import .Iterators: product
 import Distances: pairwise, Hamming
 
-
-function main(params, data)
-
     onecold(X) = map(argmax, eachrow(X))
+
+    function loss_crossentropy(Y, F)
+        ϵ = 1e-12
+        res = zeros(size(Y, 1), size(F, 1))
+        logF = log.(F .+ ϵ)
+        for i in axes(Y, 2)
+            res .+= -Y[:, i] .* logF[:, i]'
+        end
+        return res
+    end
+
+    # +
+    """
+        optimal_modality(values, loss, weight)
+
+    - values: vector of possible values
+    - weight: vector of weights 
+    - loss: matrix of size len(Weight) * len(Values)
+
+    - Returns an argmin over value in values of the scalar product <loss[value,],weight> 
+    """
+    function optimal_modality(values, loss, weight)
+
+        cost_for_each_modality = Float64[]
+        for j in eachindex(values)
+            s = 0
+            for i in axes(loss, 1)
+                s += loss[i, j] * weight[i]
+            end
+            push!(cost_for_each_modality, s)
+        end
+
+        return values[argmin(cost_for_each_modality)]
+
+    end
+
+function unbalanced_modality(params, data)
+
 
 
     base = data.database
@@ -75,25 +110,18 @@ function main(params, data)
 
     distance = Hamming()
 
-
     # Compute data for aggregation of the individuals
 
-
     Xobserv = vcat(Xhot[indA, :], Xhot[indB, :])
-    Yobserv = vcat(Y[indA], Y[indB])
-    Zobserv = vcat(Z[indA], Z[indB])
+    Yobserv = vcat(YA, YB)
+    Zobserv = vcat(ZA, ZB)
 
     nA = length(indA)
     nB = length(indB)
 
     # list the distinct modalities in A and B
-    indY = Dict((m, findall(Y[indA] .== m)) for m in Ylevels)
-    indZ = Dict((m, findall(Z[indB] .== m)) for m in Zlevels)
-
-    # compute the distance between pairs of individuals in different bases
-    # devectorize all the computations to go about twice faster only compute norm 1 here
-    a = Xhot[indA, :]'
-    b = Xhot[indB, :]'
+    indY = Dict((m, findall(YA .== m)) for m in Ylevels)
+    indZ = Dict((m, findall(ZB .== m)) for m in Zlevels)
 
     # Compute the indexes of individuals with same covariates
     indXA = Dict{Int64,Array{Int64}}()
@@ -101,16 +129,13 @@ function main(params, data)
     Xlevels = sort(unique(eachrow(Xhot)))
 
     nbX = 0
-    # aggregate both bases
     for x in Xlevels
         nbX = nbX + 1
-        distA = vec(pairwise(distance, x[:, :], a, dims = 2))
-        distB = vec(pairwise(distance, x[:, :], b, dims = 2))
+        distA = vec(pairwise(distance, x[:, :], XA', dims = 2))
+        distB = vec(pairwise(distance, x[:, :], XB', dims = 2))
         indXA[nbX] = findall(distA .< 0.1)
         indXB[nbX] = findall(distB .< 0.1)
     end
-
-    @show sort(unique(Y)), sort(unique(Z))
 
     nbX = length(indXA)
 
@@ -122,16 +147,8 @@ function main(params, data)
         z in Zlevels, x = 1:nbX
     ])
 
-    @show wa
-    @show wb
-
     wa2 = filter(>(0), wa)
     wb2 = filter(>(0), wb)
-
-    # -
-
-
-    @show length(sort(Xlevels))
 
     XYA = Vector{Int}[]
     XZB = Vector{Int}[]
@@ -146,8 +163,8 @@ function main(params, data)
     XZB2 = XZB[wb.>0] ### XZB observés
 
     # +
-    Y_hot = one_hot_encoder(Ylevels)
-    Z_hot = one_hot_encoder(Zlevels)
+    Ylevels_hot = one_hot_encoder(Ylevels)
+    Zlevels_hot = one_hot_encoder(Zlevels)
 
     nx = size(Xobserv, 2) ## Nb modalités x 
 
@@ -156,81 +173,36 @@ function main(params, data)
     # les x dans XZB observés, potentiellement des valeurs repetées 
     XB_hot = stack([v[1:nx] for v in XZB2], dims = 1)
 
-    yA = getindex.(XYA2, nx + 1)  ## les y  parmi les XYA observés, des valeurs repetées 
-    @show yA
+    yA = getindex.(XYA2, nx + 1) # les y parmi les XYA observés, potentiellement des valeurs repetées 
     yA_hot = one_hot_encoder(yA, Ylevels)
-    zB = getindex.(XZB2, nx + 1) # les z dans XZB observés, potentiellement des valeurs repetées 
-    @show zB
+    zB = getindex.(XZB2, nx + 1) # les z parmi les XZB observés, potentiellement des valeurs repetées 
     zB_hot = one_hot_encoder(zB, Zlevels)
 
-    # +
-    # ## Algorithm
-    #
-    # ### Initialisation 
-    # +
-    nA = size(XYA2, 1) # number of observed different values in A
-    nB = size(XZB2, 1) # number of observed different values in B
+    # Algorithm
+    
+    ## Initialisation 
+    
+    @show nA = size(XYA2, 1) # number of observed different values in A
+    @show nB = size(XZB2, 1) # number of observed different values in B
     nbrvarX = 3
 
     dimXZB = length(XZB2[1])
     dimXYA = length(XYA2[1])
 
-    NumberOfIterations = 3
-
     yB_pred = zeros(nB)
     zA_pred = zeros(nA)
-    function loss_crossentropy(Y, F)
-        ϵ = 1e-12
-        res = zeros(size(Y, 1), size(F, 1))
-        logF = log.(F .+ ϵ)
-        for i in axes(Y, 2)
-            res .+= -Y[:, i] .* logF[:, i]'
-        end
-        return res
-    end
-    Y_loss = loss_crossentropy(yA_hot, Y_hot)
-    Z_loss = loss_crossentropy(zB_hot, Z_hot)
 
-    # +
-    """
-        optimal_modality(values, loss, weight)
+    Yloss = loss_crossentropy(yA_hot, Ylevels_hot)
+    Zloss = loss_crossentropy(zB_hot, Zlevels_hot)
 
-    - values: vector of possible values
-    - weight: vector of weights 
-    - loss: matrix of size len(Weight) * len(Values)
+    ## Optimal Transport
 
-    - Returns an argmin over value in values of the scalar product <loss[value,],weight> 
-    """
-    function optimal_modality(values, loss, weight)
-
-        cost_for_each_modality = Float64[]
-        for j in eachindex(values)
-            s = 0
-            for i in axes(loss, 1)
-                s += loss[i, j] * weight[i]
-            end
-            push!(cost_for_each_modality, s)
-        end
-
-        return values[argmin(cost_for_each_modality)]
-
-    end
-    # -
-
-    # Zc = onehot(possible values of Z)
-    # zB2 = onehot(zB), où zB est les z dans XZB observés, 
-    # potentiellement des valeurs repetées 
-
-    # ### Optimal Transport
-
-    # +
     C0 = pairwise(Hamming(), XA_hot, XB_hot; dims = 1) .* nx ./ nbrvarX
     C = C0 ./ maximum(C0)
 
     zA_pred_hot_i = zeros(Int, (nA_i, length(Zlevels)))
     yB_pred_hot_i = zeros(Int, (nB_i, length(Ylevels)))
 
-    # +
     NumberOfIterations = 10
 
     for iter = 1:NumberOfIterations
@@ -238,7 +210,7 @@ function main(params, data)
         G = PythonOT.mm_unbalanced(wa2, wb2, C, 0.1; div = "kl") #unbalanced
 
         for j in eachindex(yB_pred)
-            yB_pred[j] = optimal_modality(Ylevels, Y_loss, G[:, j])
+            yB_pred[j] = optimal_modality(Ylevels, Yloss, view(G,:, j))
         end
 
 
@@ -247,7 +219,7 @@ function main(params, data)
         ### Compute best g: XxY-->Z
 
         for i in eachindex(zA_pred)
-            zA_pred[i] = optimal_modality(Zlevels, Z_loss, G[i, :])
+            zA_pred[i] = optimal_modality(Zlevels, Zloss, G[i, :])
         end
 
 
@@ -261,28 +233,18 @@ function main(params, data)
         chinge2 = alpha2 * loss_crossentropy(zB_hot, zA_pred_hot)
         fcost = chinge1 .+ chinge2'
 
-
         C .= C0 ./ maximum(C0) .+ fcost
-
 
         ### Predict
 
         for i in axes(XYA_i, 1)
             ind = findfirst(XYA_i[i, :] == v for v in XYA2)
-            if isnothing(ind)
-                zA_pred_hot_i[i, :] .= 0
-            else
-                zA_pred_hot_i[i, :] .= zA_pred_hot[ind, :]
-            end
+            zA_pred_hot_i[i, :] .= zA_pred_hot[ind, :]
         end
 
         for i in axes(XZB_i, 1)
             ind = findfirst(XZB_i[i, :] == v for v in XZB2)
-            if isnothing(ind)
-                yB_pred_hot_i[i, :] .= 0
-            else
-                yB_pred_hot_i[i, :] .= yB_pred_hot[ind, :]
-            end
+            yB_pred_hot_i[i, :] .= yB_pred_hot[ind, :]
         end
 
 
@@ -304,10 +266,10 @@ params = DataParameters(nA = 1000, nB = 1000, mB = [2, 0, 0], eps = 0.0, p = 0.2
 # data = generate_xcat_ycat(params)
 data = CSV.read(joinpath(@__DIR__, "data_good.csv"), DataFrame)
 @show sort(unique(data.Y)), sort(unique(data.Z))
-@time main(params, data)
+@time unbalanced_modality(params, data)
 
 params = DataParameters(nA = 1000, nB = 1000, mB = [0, 0, 0], eps = 0.0, p = 0.2)
 data = CSV.read(joinpath(@__DIR__, "data_bad.csv"), DataFrame)
 @show sort(unique(data.Y)), sort(unique(data.Z))
 
-@time main(params, data)
+@time unbalanced_modality(params, data)
