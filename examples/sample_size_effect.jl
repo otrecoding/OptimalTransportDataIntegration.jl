602b42a3446eacb2d98ec53596671080cfe9034e
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-12-09 15:16:46 +0100

    Create the sample size effect simulation

--- examples/sample_size_effect.jl
+++ examples/sample_size_effect.jl
@@ -18,43 +18,29 @@ using DelimitedFiles
 using OptimalTransportDataIntegration
 
 # +
-function sample_size_effect(all_params::Vector{DataParameters}, nsimulations::Int)
+function sample_size_effect(all_params, nsimulations)
 
-    outfile = "sample_size_effect_ot.csv"
-    header = ["id", "nA", "nB", "estimation", "method"]
+    estimations = Float32[]
+    
+    outfile =  "sample_size_effect_ot.csv"
+    header = ["id", "nA", "nB", "maxrelax", "lambda_reg", "estimation"]
 
-    open(outfile, "w") do io
+    open(outfile, "a") do io
 
+        seekstart(io)
         writedlm(io, hcat(header...))
-
         for params in all_params
 
-            for i = 1:nsimulations
-
+            for i in 1:nsimulations
+            
                 data = generate_xcat_ycat(params)
-
-                #OT Transport of the joint distribution of covariates and outcomes.
-                maxrelax, lambda_reg = 0.0, 0.0
+                maxrelax,  lambda_reg = 0.0, 0.0
                 est = otrecod(data, OTjoint(maxrelax = maxrelax, lambda_reg = lambda_reg))
-                writedlm(io, [i params.nA params.nB est "ot"])
-
-                #OT-r Regularized Transport 
-                maxrelax, lambda_reg = 0.4, 0.1
+                writedlm(io, [i params.nA params.nB  maxrelax lambda_reg est ])
+                maxrelax,  lambda_reg = 0.4, 0.1
                 est = otrecod(data, OTjoint(maxrelax = maxrelax, lambda_reg = lambda_reg))
-                writedlm(io, [i params.nA params.nB est "ot-r"])
-
-                #OTE Balanced transport of covariates and estimated outcomes
-                est = otrecod(data, UnbalancedModality(reg = 0.01, reg_m = 0.0))
-                writedlm(io, [i params.nA params.nB est "ote"])
-
-                #OTE Regularized unbalanced transport 
-                est = otrecod(data, UnbalancedModality(reg = 0.01, reg_m = 0.05))
-                writedlm(io, [i params.nA params.nB est "ote-r"])
-
-                #SL Simple Learning
-                est = otrecod(data, SimpleLearning())
-                writedlm(io, [i params.nA params.nB est "sl"])
-
+                writedlm(io, [i params.nA params.nB  maxrelax lambda_reg est ])
+            
             end
 
         end
@@ -63,12 +49,12 @@ function sample_size_effect(all_params::Vector{DataParameters}, nsimulations::In
 
 end
 
-all_params = [
+all_params = (
     DataParameters(nA = 100, nB = 100),
     DataParameters(nA = 1000, nB = 1000),
-    DataParameters(nA = 10000, nB = 10000),
-]
+    DataParameters(nA = 10000, nB = 10000)
+)
 
 nsimulations = 100
 
-@time sample_size_effect(all_params, nsimulations)
+@time sample_size_effect(nsimulations, all_params)
