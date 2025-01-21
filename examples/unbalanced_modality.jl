0d87636596239781ab3922bc6d2b176c460ed845
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-01-21 17:01:47 +0100

    Update unbalanced_modality.jl

--- examples/unbalanced_modality.jl
+++ examples/unbalanced_modality.jl
@@ -22,6 +22,9 @@ using OptimalTransportDataIntegration
 params = DataParameters(nA = 1000, nB = 1000, mB = [1, 0, 0], eps = 0.0, p = 0.2)
 data = generate_xcat_ycat(params)
 
+est = otrecod(data, OTjoint())
+println("OT-r : $est")
+
 est = otrecod(data, UnbalancedModality(iterations = 10))
 println("OTE-r : $est")
 
@@ -31,6 +34,8 @@ println("OTE : $est")
 params = DataParameters(nA = 1000, nB = 1000, mB = [2, 0, 0], eps = 0.0, p = 0.2)
 data = generate_xcat_ycat(params)
 
+est = otrecod(data, OTjoint())
+println("OT-r : $est")
 est = otrecod(data, UnbalancedModality(iterations = 10))
 println("OTE-r : $est")
 
@@ -38,14 +43,20 @@ est = otrecod(data, UnbalancedModality(reg_m = 0.0))
 println("OTE : $est")
 
 data = CSV.read(joinpath(@__DIR__, "../test/data_good.csv"), DataFrame)
+est = otrecod(data, OTjoint())
+println("OT-r : $est")
 est = otrecod(data, UnbalancedModality(iterations = 10))
 println("OTE-r : $est")
 
 data = CSV.read(joinpath(@__DIR__, "../test/data_bad.csv"), DataFrame)
+est = otrecod(data, OTjoint())
+println("OT-r : $est")
 est = otrecod(data, UnbalancedModality(iterations = 10))
 println("OTE-r : $est")
 
 params = DataParameters(nA = 1000, nB = 500)
 data = generate_xcat_ycat(params)
+est = otrecod(data, OTjoint())
+println("OT-r : $est")
 est = otrecod(data, UnbalancedModality(iterations = 10))
 println("OTE-r : $est")
