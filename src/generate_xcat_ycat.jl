050e15d23f94d334586520878991645ead5e8e6c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-04-14 16:20:54 +0200

    Update generate_xcat_ycat.jl

--- src/generate_xcat_ycat.jl
+++ src/generate_xcat_ycat.jl
@@ -3,7 +3,7 @@ using DataFrames
 
 export generate_xcat_ycat
 
-function generate_XY(params, bins11, bins12, bins13)
+function generate_XY(params, binsA1, binsA2, binsA3, binsB1, binsB2, binsB3)
 
     dA = MvNormal(params.mA, params.covA)
     dB = MvNormal(params.mB, params.covB)
@@ -15,28 +15,38 @@ function generate_XY(params, bins11, bins12, bins13)
     px2cc = cumsum(params.px2c)[1:end-1]
     px3cc = cumsum(params.px3c)[1:end-1]
 
-    qx1c = quantile.(Normal(0.0, 1.0), px1cc)
-    qx2c = quantile.(Normal(0.0, 1.0), px2cc)
-    qx3c = quantile.(Normal(0.0, 1.0), px3cc)
+    qxA1c = quantile.(Normal(params.mA[1], 1.0), px1cc)
+    qxA2c = quantile.(Normal(params.mA[2], 1.0), px2cc)
+    qxA3c = quantile.(Normal(params.mA[3], 1.0), px3cc)
 
-    bins11 = vcat(minimum(X_glob1[1, :]) - 100, qx1c, maximum(X_glob1[1, :]) + 100)
-    bins12 = vcat(minimum(X_glob1[2, :]) - 100, qx2c, maximum(X_glob1[2, :]) + 100)
-    bins13 = vcat(minimum(X_glob1[3, :]) - 100, qx3c, maximum(X_glob1[3, :]) + 100)
+    qxB1c = quantile.(Normal(params.mB[1], 1.0), px1cc)
+    qxB2c = quantile.(Normal(params.mB[2], 1.0), px2cc)
+    qxB3c = quantile.(Normal(params.mB[3], 1.0), px3cc)
 
-    X11 = digitize(X_glob1[1, :], bins11)
-    X12 = digitize(X_glob1[2, :], bins12)
-    X13 = digitize(X_glob1[3, :], bins13)
 
-    X21 = digitize(X_glob2[1, :], bins11)
-    X22 = digitize(X_glob2[2, :], bins12)
-    X23 = digitize(X_glob2[3, :], bins13)
+    binsA1 = vcat(minimum(X_glob1[1, :]) - 100, qxA1c, maximum(X_glob1[1, :]) + 100)
+    binsA2 = vcat(minimum(X_glob1[2, :]) - 100, qxA2c, maximum(X_glob1[2, :]) + 100)
+    binsA3 = vcat(minimum(X_glob1[3, :]) - 100, qxA3c, maximum(X_glob1[3, :]) + 100)
 
-    X11c = to_categorical(X11, 1:2)[2:end, :]
-    X21c = to_categorical(X21, 1:2)[2:end, :]
-    X12c = to_categorical(X12, 1:3)[2:end, :]
-    X22c = to_categorical(X22, 1:3)[2:end, :]
-    X13c = to_categorical(X13, 1:4)[2:end, :]
-    X23c = to_categorical(X23, 1:4)[2:end, :]
+    binsB1 = vcat(minimum(X_glob2[1, :]) - 100, qxB1c, maximum(X_glob2[1, :]) + 100)
+    binsB2 = vcat(minimum(X_glob2[2, :]) - 100, qxB2c, maximum(X_glob2[2, :]) + 100)
+    binsB3 = vcat(minimum(X_glob2[3, :]) - 100, qxB3c, maximum(X_glob2[3, :]) + 100)
+
+
+    X11 = digitize(X_glob1[1, :], binsA1)
+    X12 = digitize(X_glob1[2, :], binsA2)
+    X13 = digitize(X_glob1[3, :], binsA3)
+
+    X21 = digitize(X_glob2[1, :], binsB1)
+    X22 = digitize(X_glob2[2, :], binsB2)
+    X23 = digitize(X_glob2[3, :], binsB3)
+
+    X11c = to_categorical(X11)[2:end, :]
+    X21c = to_categorical(X21)[2:end, :]
+    X12c = to_categorical(X12)[2:end, :]
+    X22c = to_categorical(X22)[2:end, :]
+    X13c = to_categorical(X13)[2:end, :]
+    X23c = to_categorical(X23)[2:end, :]
 
     X1 = vcat(X11, X21)
     X2 = vcat(X12, X22)
@@ -46,7 +56,7 @@ function generate_XY(params, bins11, bins12, bins13)
     Y2 = vcat(X21c, X22c, X23c)' * params.aB
 
     X1, X2, X3, Y1, Y2
-
+#####ou X11, X21,X12, X22,X13, X23
 end
 
 function generate_YZ(params, Y1, Y2)
@@ -102,23 +112,23 @@ function generate_YZ(params, Y1, Y2)
 
 end
 
-function generate_dataframe(params, bins11, bins12, bins13, binsY11, binsY22)
+function generate_dataframe(params, binsA1, binsA2, binsA3, binsB1, binsB2, binsB3, binsYA1, binsYB1, binsYA2, binsYB2)
 
-    X1, X2, X3, Y1, Y2 = generate_XY(params, bins11, bins12, bins13)
+    X1, X2, X3, Y1, Y2 = generate_XY(params, binsA1, binsA2, binsA3, binsB1, binsB2, binsB3)
     Y, Z = generate_YZ(params, Y1, Y2)
 
-    Y11 = digitize(Y, binsY11)
-    Y12 = digitize(Y, binsY22)
+    YA1 = digitize(Y, binsYA1)
+    YA2 = digitize(Y, binsYA2)
 
-    binsY11eps = binsY11 .+ params.eps
-    binsY22eps = binsY22 .+ params.eps
+    binsYB1eps = binsYB1 .+ params.eps
+    binsYB2eps = binsYB2 .+ params.eps
 
-    Y21 = digitize(Z, binsY11eps)
-    Y22 = digitize(Z, binsY22eps)
+    YB1 = digitize(Z, binsYB1eps)
+    YB2 = digitize(Z, binsYB2eps)
 
     df = DataFrame(hcat(X1, X2, X3) .- 1, [:X1, :X2, :X3])
-    df.Y = vcat(Y11, Y21)
-    df.Z = vcat(Y12, Y22)
+    df.Y = vcat(YA1, YB1)
+    df.Z = vcat(YA2, YB2)
     df.database = vcat(fill(1, params.nA), fill(2, params.nB))
 
     return df
@@ -144,24 +154,41 @@ function generate_xcat_ycat(params::DataParameters)
     px2cc = cumsum(params.px2c)[1:end-1]
     px3cc = cumsum(params.px3c)[1:end-1]
 
-    qx1c = quantile.(Normal(0.0, 1.0), px1cc)
-    qx2c = quantile.(Normal(0.0, 1.0), px2cc)
-    qx3c = quantile.(Normal(0.0, 1.0), px3cc)
+    qxA1c = quantile.(Normal(params.mA[1], 1.0), px1cc)
+    qxA2c = quantile.(Normal(params.mA[2], 1.0), px2cc)
+    qxA3c = quantile.(Normal(params.mA[3], 1.0), px3cc)
 
-    bins11 = vcat(minimum(XA[1, :]) - 100, qx1c, maximum(XA[1, :]) + 100)
-    bins12 = vcat(minimum(XA[2, :]) - 100, qx2c, maximum(XA[2, :]) + 100)
-    bins13 = vcat(minimum(XA[3, :]) - 100, qx3c, maximum(XA[3, :]) + 100)
+    qxB1c = quantile.(Normal(params.mB[1], 1.0), px1cc)
+    qxB2c = quantile.(Normal(params.mB[2], 1.0), px2cc)
+    qxB3c = quantile.(Normal(params.mB[3], 1.0), px3cc)
 
-    X1, X2, X3, Y1, Y2 = generate_XY(params, bins11, bins12, bins13)
+    binsA1 = vcat(minimum(XA[1, :]) - 100, qxA1c, maximum(XA[1, :]) + 100)
+    binsA2 = vcat(minimum(XA[2, :]) - 100, qxA2c, maximum(XA[2, :]) + 100)
+    binsA3 = vcat(minimum(XA[3, :]) - 100, qxA3c, maximum(XA[3, :]) + 100)
+
+    binsB1 = vcat(minimum(XB[1, :]) - 100, qxB1c, maximum(XB[1, :]) + 100)
+    binsB2 = vcat(minimum(XB[2, :]) - 100, qxB2c, maximum(XB[2, :]) + 100)
+    binsB3 = vcat(minimum(XB[3, :]) - 100, qxB3c, maximum(XB[3, :]) + 100)
+
+
+
+    X1, X2, X3, Y1, Y2 = generate_XY(params, binsA1, binsA2, binsA3, binsB1, binsB2, binsB3)
 
     Y, Z = generate_YZ(params, Y1, Y2)
-    b11 = quantile(Y, [0.25, 0.5, 0.75])
-    b22 = quantile(Z, [1 / 3, 2 / 3])
+    bA1 = quantile(Y, [0.25, 0.5, 0.75])
+    bB1 = quantile(Z, [0.25, 0.5, 0.75])
+    bA2 = quantile(Y, [1 / 3, 2 / 3])
+    bB2 = quantile(Z, [1 / 3, 2 / 3])
 
-    binsY11 = vcat(minimum(Y) - 100, b11, maximum(Y) + 100)
-    binsY22 = vcat(minimum(Z) - 100, b22, maximum(Z) + 100)
 
-    df = generate_dataframe(params, bins11, bins12, bins13, binsY11, binsY22)
+    binsYA1 = vcat(minimum(Y) - 100, bA1, maximum(Y) + 100)
+    binsYB1 = vcat(minimum(Y) - 100, bB1, maximum(Y) + 100)
+
+    binsYA2 = vcat(minimum(Z) - 100, bA2, maximum(Z) + 100)
+    binsYB2 = vcat(minimum(Z) - 100, bB2, maximum(Z) + 100)
+
+
+    df = generate_dataframe(params, binsA1, binsA2, binsA3, binsB1, binsB2, binsB3, binsYA1, binsYB1, binsYA2, binsYB2)
 
     my = length(unique(df.Y))
     if my < 4
