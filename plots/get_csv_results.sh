d541c78cfa343aab713da174b394b9a44627fcec
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-05-28 09:26:48 +0200

    remove plots directory

--- plots/get_csv_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-scp tesla-irmar:OptimalTransportDataIntegration.jl/conditional_distribution.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/covariates_link_effect.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/covariates_shift_assumption.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/results_otjoint.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/results_simple_learning.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/results_unbalanced.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/sample_ratio_effect.csv .
-scp tesla-irmar:OptimalTransportDataIntegration.jl/sample_size_effect.csv .
