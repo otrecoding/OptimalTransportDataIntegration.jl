efdd5b62537678d0a0de92648ce5296034da5ff6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-09-02 13:41:27 +0200

    Update ref_methods_examples.jl

--- examples/ref_methods_examples.jl
+++ examples/ref_methods_examples.jl
@@ -1,14 +1,16 @@
 using OptimalTransportDataIntegration
 
-params = DataParameters()
+params = DataParameters(mB = [5, 5, 5], aA = [1.,1,1], aB = [1.,1,1])
 
-rng = DataGenerator(params, discrete = false)
+rng = DataGenerator(params, scenario = 2, discrete = false)
 
 data = generate( rng ) 
 
 
-result = otrecod( data, JointOTBetweenBasesRefJDOT())
-println("JointOTBetweenBasesRefJDOT : $(accuracy(result))")
+println(data)
+
+# result = otrecod( data, JointOTBetweenBasesRefJDOT())
+# println("JointOTBetweenBasesRefJDOT : $(accuracy(result))")
 
 # result = otrecod( data, JointOTBetweenBasesrefOTDAx())
 # println("JointOTBetweenBasesrefOTDAx : $(accuracy(result))")
