b9c3229a70d2fe454fe1bdc2925c96dccc077e81
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-09-05 21:13:37 +0200

    Update one_dim_covariates.jl

--- examples/one_dim_covariates.jl
+++ examples/one_dim_covariates.jl
@@ -1,6 +1,33 @@
 using OptimalTransportDataIntegration
 using Test
 
+params = DataParameters()  # X1, X2, X3
+
+rng = DiscreteDataGenerator(params)
+data = generate(rng)
+@show accuracy(otrecod(data, SimpleLearning()))
+
+rng = ContinuousDataGenerator(params)
+data = generate(rng)
+@show accuracy(otrecod(data, SimpleLearning()))
+
+# Only X1
+params = DataParameters(mA = [0.0], 
+                        mB = [0.0],
+                        covA = ones(1,1),
+                        covB = ones(1,1),
+                        aA = [1.0],
+                        aB = [1.0])
+ 
+rng = DiscreteDataGenerator(params)
+data = generate(rng)
+@show accuracy(otrecod(data, SimpleLearning()))
+ 
+rng = ContinuousDataGenerator(params)
+ata = generate(rng)
+@show accuracy(otrecod(data, SimpleLearning()))
+
+# X1, X2
 params = DataParameters(mA = [0.0, 0.0], mB = [0.0, 0.0],
                         covA = [1.0 0.2; 0.2 1.0],
                         covB = [1.0 0.2; 0.2 1.0],
@@ -9,28 +36,6 @@ params = DataParameters(mA = [0.0, 0.0], mB = [0.0, 0.0],
                         aA = [1.0, 1.0],
                         aB = [1.0, 1.0])
 
-OptimalTransportDataIntegration.save("test.json", params)
-@show OptimalTransportDataIntegration.read("test.json")
-
-println("discrete data with default params")
-params = DataParameters()
-
-rng = DiscreteDataGenerator(params)
-data = generate(rng)
-@show accuracy(otrecod(data, SimpleLearning()))
-
-println("continuous data with default params")
-
-rng = ContinuousDataGenerator(params)
-data = generate(rng)
-@show accuracy(otrecod(data, SimpleLearning()))
-
-params = DataParameters(mA = [0.0], mB = [0.0],
-                        covA = ones(1,1),
-                        covB = ones(1,1),
-                        aA = [1.0],
-                        aB = [1.0])
- 
 rng = DiscreteDataGenerator(params)
 data = generate(rng)
 @show accuracy(otrecod(data, SimpleLearning()))
