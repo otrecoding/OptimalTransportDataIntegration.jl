af9e38933ac726a86e1d86829c81a154154ba132
vagares <44903659+vagares@users.noreply.github.com>
2025-09-18 14:51:29 +0200

    Update OptimalTransportDataIntegration.jl

--- src/OptimalTransportDataIntegration.jl
+++ src/OptimalTransportDataIntegration.jl
@@ -24,7 +24,7 @@ include("entropic_partial_wasserstein.jl")
 include("joint_ot_between_bases.jl")
 include("joint_ot_between_bases_with_predictors.jl")
 include("joint_ot_between_without_yz.jl")
-
+include("joint_ot_between_bases_c.jl")
 include("simple_learning.jl")
 include("simple_learning_with_continuous_data.jl")
 
